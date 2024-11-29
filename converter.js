c2b1e5b5379d64780580040669c0c5ca4589f3f9
Samriddhi1102022 <tiwarisamriddhi1@gmail.com>
2024-11-29 23:43:58 +0530

    add minor changes

--- converter.js
+++ converter.js
@@ -6,27 +6,27 @@ const resultElement = document.getElementById('result');
 
 const exchangeRates = {
 	USD: {
-		EUR: 0.88,
-		GBP: 0.76,
-		INR: 74.83,
+		EUR: 0.95,
+		GBP: 0.79,
+		INR: 84.56,
 		// Add more exchange rates
 	},
 	EUR: {
-		USD: 1.13,
-		GBP: 0.86,
-		INR: 84.81,
+		USD: 1.06,
+		GBP: 0.83,
+		INR: 89.45,
 		// Add more exchange rates
 	},
 	GBP: {
-		USD: 1.31,
-		EUR: 1.16,
-		INR: 98.41,
+		USD: 1.27,
+		EUR: 1.20,
+		INR: 107.67,
 		// Add more exchange rates
 	},
     INR: {
-		USD: 0.013,
-		EUR: 0.012,
-		GBP: 0.010,
+		USD: 0.012,
+		EUR: 0.011,
+		GBP: 0.0093,
 		// Add more exchange rates
 	},
 	// Add more currencies
